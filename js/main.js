8289a874cb22f8398ad121ae16ecb87d9e66d5ad
dapengme <imzhpe@qq.com>
2012-10-01 17:22:05 +0800

    bootstrap for diandian 主题

---
+++ js/main.js
@@ -0,0 +1,9 @@
+  jQuery(document).ready(function($) {
+  	$(".post").hover(function() {
+  		$(this).find(".read-more").fadeToggle();
+  	});
+
+    $('a').tooltip({
+    	placement:"top"
+    });
+  });
